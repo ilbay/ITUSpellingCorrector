c0313f1fa287f55abdf982f86e11c9b86de27f2b
ilbay <tugay.ilbay@gmail.com>
2014-03-04 12:51:38 +0200

    Computing min edit distance method is implemented. (iss1)

--- src/main/java/tr/edu/itu/bb/spellcorrection/levenshtein/LevenshteinDistance.java
+++ src/main/java/tr/edu/itu/bb/spellcorrection/levenshtein/LevenshteinDistance.java
@@ -29,7 +29,7 @@ public class LevenshteinDistance {
 				if(s1.charAt(i-1)==s2.charAt(j-1)){
 					d[i][j]=d[i-1][j-1];
 				}else{
-					d[i][j]=Math.min(d[i-1][j]+1, Math.min(d[i][j-1]+1, d[i-1][j-1]+2));
+					d[i][j]=Math.min(d[i-1][j]+1, Math.min(d[i][j-1]+1, d[i-1][j-1]+1));
 				}
 			}
 		}
